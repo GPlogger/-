8aa6c6fe667fe86fae4b4c3c4560b28f66ee0ed4
DESKTOP-UATT97O\kkjj4 <gmpin12@gmail.com>
2022-11-12 08:08:41 +0900

    React 파일 업로드

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 리액트 데이터 저장 공간 [state데이터, 데이터 변경 함수], 안에 배열, 객체 등 여러 형태 넣을 수 있음
+  // state는 값이 변경되면 새로고침 없이도 HTML이 자동으로 재랜더링이 됨
+  let [placeTitle, setPlaceTitle] = useState('제주공항');
+
+  function addScadule() {console.log(1);}
+  // 데이터 바인딩이 매우 쉬움 ( {변수명} 을 사용해 랜더링 시킬 수 있음)
+  // {} 안에 변수명 또는 함수() 를 사용
+  // 스타일 넣을 때 오브젝트 형식( style={ color : blue}) 으로 넣어야 함
+    return (
+    <div className="App">
+      <div className="black-nav">
+        <div><img src="assets/img/logo.png" alt="logo" className="logo-img"/></div>
+      </div>
+      <div className="List">
+        <h3>{placeTitle} <span onClick={addScadule}>✔</span></h3>
+        <p>제주공항입니다.</p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StartPage/StartPage.jsx
@@ -0,0 +1,357 @@
+<div
+  class="daterangepicker ltr show-calendar opensleft"
+  style="top: 239.2px; right: 528.2px; left: auto; display: block;"
+>
+  <div class="ranges"></div>
+  <div id="drpcalleft" class="drp-calendar left">
+    <div class="calendar-table">
+      <table class="table-condensed">
+        <thead>
+          <tr>
+            <th class="prev available">
+              <span></span>
+            </th>
+            <th colspan="5" class="month">
+              11월 2022
+            </th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>일</th>
+            <th>월</th>
+            <th>화</th>
+            <th>수</th>
+            <th>목</th>
+            <th>금</th>
+            <th>토</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="weekend off available" data-title="r0c0">
+              30
+            </td>
+            <td class="off available" data-title="r0c1">
+              31
+            </td>
+            <td class="available" data-title="r0c2">
+              1
+            </td>
+            <td class="available" data-title="r0c3">
+              2
+            </td>
+            <td class="available" data-title="r0c4">
+              3
+            </td>
+            <td class="available" data-title="r0c5">
+              4
+            </td>
+            <td class="weekend available" data-title="r0c6">
+              5
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r1c0">
+              6
+            </td>
+            <td class="available" data-title="r1c1">
+              7
+            </td>
+            <td class="available" data-title="r1c2">
+              8
+            </td>
+            <td class="available" data-title="r1c3">
+              9
+            </td>
+            <td class="available" data-title="r1c4">
+              10
+            </td>
+            <td class="available" data-title="r1c5">
+              11
+            </td>
+            <td class="today weekend available" data-title="r1c6">
+              12
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r2c0">
+              13
+            </td>
+            <td class="available" data-title="r2c1">
+              14
+            </td>
+            <td class="available" data-title="r2c2">
+              15
+            </td>
+            <td class="available" data-title="r2c3">
+              16
+            </td>
+            <td class="available" data-title="r2c4">
+              17
+            </td>
+            <td class="available" data-title="r2c5">
+              18
+            </td>
+            <td class="weekend active start-date available" data-title="r2c6">
+              19
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend in-range available" data-title="r3c0">
+              20
+            </td>
+            <td class="active end-date in-range available" data-title="r3c1">
+              21
+            </td>
+            <td class="available" data-title="r3c2">
+              22
+            </td>
+            <td class="available" data-title="r3c3">
+              23
+            </td>
+            <td class="available" data-title="r3c4">
+              24
+            </td>
+            <td class="available" data-title="r3c5">
+              25
+            </td>
+            <td class="weekend available" data-title="r3c6">
+              26
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r4c0">
+              27
+            </td>
+            <td class="available" data-title="r4c1">
+              28
+            </td>
+            <td class="available" data-title="r4c2">
+              29
+            </td>
+            <td class="available" data-title="r4c3">
+              30
+            </td>
+            <td class="off available" data-title="r4c4">
+              1
+            </td>
+            <td class="off available" data-title="r4c5">
+              2
+            </td>
+            <td class="weekend off available" data-title="r4c6">
+              3
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend off available" data-title="r5c0">
+              4
+            </td>
+            <td class="off available" data-title="r5c1">
+              5
+            </td>
+            <td class="off available" data-title="r5c2">
+              6
+            </td>
+            <td class="off available" data-title="r5c3">
+              7
+            </td>
+            <td class="off available" data-title="r5c4">
+              8
+            </td>
+            <td class="off available" data-title="r5c5">
+              9
+            </td>
+            <td class="weekend off available" data-title="r5c6">
+              10
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="calendar-time" style="display: none;"></div>
+  </div>
+  <div id="drpcalright" class="drp-calendar right">
+    <div class="calendar-table">
+      <table class="table-condensed">
+        <thead>
+          <tr>
+            <th></th>
+            <th colspan="5" class="month">
+              12월 2022
+            </th>
+            <th class="next available">
+              <span></span>
+            </th>
+          </tr>
+          <tr>
+            <th>일</th>
+            <th>월</th>
+            <th>화</th>
+            <th>수</th>
+            <th>목</th>
+            <th>금</th>
+            <th>토</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="weekend off available" data-title="r0c0">
+              27
+            </td>
+            <td class="off available" data-title="r0c1">
+              28
+            </td>
+            <td class="off available" data-title="r0c2">
+              29
+            </td>
+            <td class="off available" data-title="r0c3">
+              30
+            </td>
+            <td class="available" data-title="r0c4">
+              1
+            </td>
+            <td class="available" data-title="r0c5">
+              2
+            </td>
+            <td class="weekend available" data-title="r0c6">
+              3
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r1c0">
+              4
+            </td>
+            <td class="available" data-title="r1c1">
+              5
+            </td>
+            <td class="available" data-title="r1c2">
+              6
+            </td>
+            <td class="available" data-title="r1c3">
+              7
+            </td>
+            <td class="available" data-title="r1c4">
+              8
+            </td>
+            <td class="available" data-title="r1c5">
+              9
+            </td>
+            <td class="weekend available" data-title="r1c6">
+              10
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r2c0">
+              11
+            </td>
+            <td class="available" data-title="r2c1">
+              12
+            </td>
+            <td class="available" data-title="r2c2">
+              13
+            </td>
+            <td class="available" data-title="r2c3">
+              14
+            </td>
+            <td class="available" data-title="r2c4">
+              15
+            </td>
+            <td class="available" data-title="r2c5">
+              16
+            </td>
+            <td class="weekend available" data-title="r2c6">
+              17
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r3c0">
+              18
+            </td>
+            <td class="available" data-title="r3c1">
+              19
+            </td>
+            <td class="available" data-title="r3c2">
+              20
+            </td>
+            <td class="available" data-title="r3c3">
+              21
+            </td>
+            <td class="available" data-title="r3c4">
+              22
+            </td>
+            <td class="available" data-title="r3c5">
+              23
+            </td>
+            <td class="weekend available" data-title="r3c6">
+              24
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend available" data-title="r4c0">
+              25
+            </td>
+            <td class="available" data-title="r4c1">
+              26
+            </td>
+            <td class="available" data-title="r4c2">
+              27
+            </td>
+            <td class="available" data-title="r4c3">
+              28
+            </td>
+            <td class="available" data-title="r4c4">
+              29
+            </td>
+            <td class="available" data-title="r4c5">
+              30
+            </td>
+            <td class="weekend available" data-title="r4c6">
+              31
+            </td>
+          </tr>
+          <tr>
+            <td class="weekend off available" data-title="r5c0">
+              1
+            </td>
+            <td class="off available" data-title="r5c1">
+              2
+            </td>
+            <td class="off available" data-title="r5c2">
+              3
+            </td>
+            <td class="off available" data-title="r5c3">
+              4
+            </td>
+            <td class="off available" data-title="r5c4">
+              5
+            </td>
+            <td class="off available" data-title="r5c5">
+              6
+            </td>
+            <td class="weekend off available" data-title="r5c6">
+              7
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="calendar-time" style="display: none;"></div>
+  </div>
+  <div class="drp-buttons">
+    <span class="drp-selected">2022.11.19 - 2022.11.21</span>
+    <button
+      class="cancelBtn btn btn-sm btn-default"
+      type="button"
+      style="font-size:12px;line-height:0;font-weight: 400;"
+    >
+      닫기
+    </button>
+    <button
+      class="applyBtn btn btn-sm btn-primary"
+      type="button"
+      style="font-size:12px;line-height:0;font-weight: 400"
+    >
+      적용하기
+    </button>{" "}
+  </div>
+</div>;
